6a746a61e833ae198309e70273f217f7c657d16d
Avish kumar <avishkmonga@gmail.com>
2024-03-31 19:05:07 +0530

    stream client added

---
+++ actions/stream.actions.ts
@@ -0,0 +1,19 @@
+"use server";
+
+import { currentUser } from "@clerk/nextjs";
+import { StreamClient } from "@stream-io/node-sdk";
+
+const apiKey = process.env.NEXT_PUBLIC_STREAM_API_KEY;
+const apiSecret = process.env.STREAM_SECRET_KEY;
+
+export const tokenProvider = async () => {
+  const user = await currentUser();
+  if (!user) throw new Error("User must be logged in");
+  if (!apiKey || !apiSecret)
+    throw new Error("Stream API key and secret must be provided");
+  const streamClient = new StreamClient(apiKey, apiSecret);
+  const exp = Math.round(new Date().getTime() / 1000) + 60 * 60;
+  const issued = Math.floor(Date.now() / 1000) - 60;
+  const token = streamClient.createToken(user.id, exp, issued);
+  return token;
+};
--- components/MeetingTypeList.tsx
+++ components/MeetingTypeList.tsx
@@ -3,13 +3,51 @@ import React, { useState } from "react";
 import HomeCard from "./HomeCard";
 import { useRouter } from "next/navigation";
 import MeetingModal from "./MeetingModal";
+import { useUser } from "@clerk/nextjs";
+import { Call, useStreamVideoClient } from "@stream-io/video-react-sdk";
+import { useToast } from "./ui/use-toast";
 
 const MeetingTypeList = () => {
   const router = useRouter();
+  const { toast } = useToast();
   const [meetingState, setMeetingState] = useState<
     "isScheduleMeeting" | "isJoiningMeeting" | "isInstantMeeting" | undefined
   >();
-  const createMeeting = () => {};
+  const [values, setValues] = useState({
+    dateTime: new Date(),
+    description: "",
+    link: "",
+  });
+  const [callDetails, setCallDetails] = useState<Call>();
+  const { user } = useUser();
+  const client = useStreamVideoClient();
+  const createMeeting = async () => {
+    if (!user || !client) return;
+    try {
+      let id = crypto.randomUUID();
+      const call = client.call("default", id);
+      if (!call) throw new Error("Failed to create call");
+      const startsAt =
+        values.dateTime.toISOString() || new Date(Date.now()).toISOString();
+      const description = values.description || "Instant Meeting";
+      await call.getOrCreate({
+        data: {
+          starts_at: startsAt,
+          custom: {
+            description,
+          },
+        },
+      });
+      setCallDetails(call);
+      if (!values.description) {
+        router.push(`/meeting/${call.id}`);
+      }
+      toast({ title: "Meeting created" });
+    } catch (err) {
+      console.log(err);
+      toast({ title: "Failed to create meeting", variant: "destructive" });
+    }
+  };
   return (
     <section className="grid grid-cols-1 gap-5 md:grid-cols-2 xl:grid-cols-4">
       <HomeCard
